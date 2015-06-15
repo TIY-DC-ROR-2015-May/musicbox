9407ac3b026fec80d01f0487e75b66186e7b3090
James Dabbs <jamesdabbs@gmail.com>
2015-06-15 11:50:10 -0400

    Working user sign in system

--- server.rb
+++ server.rb
@@ -2,15 +2,42 @@ require 'sinatra/base'
 
 require 'pry'
 
+require './db/setup'
+require './lib/all'
+
 class MusicBoxApp < Sinatra::Base
   enable :logging
+  enable :sessions
+
+  def current_user
+    if session[:logged_in_user_id]
+      User.find session[:logged_in_user_id]
+    #else
+    #  nil
+    end
+  end
 
   get "/sign_in" do
     erb :sign_in
   end
 
   post "/take_sign_in" do
-    binding.pry
+    user = User.where(
+      name:     params[:username],
+      password: params[:password]
+    ).first
+
+    if user
+      session[:logged_in_user_id] = user.id
+      redirect to("/")
+    else
+      # TODO: Set a message bad password ...
+      redirect to("/sign_in")
+    end
+  end
+
+  get "/" do
+    erb :home
   end
 end
 
