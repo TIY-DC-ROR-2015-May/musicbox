4147b8ee5d47bb5e122a89409dfc96802b1438de
James Dabbs <jamesdabbs@gmail.com>
2015-06-15 13:24:58 -0400

    Add tests

--- db/setup.rb
+++ db/setup.rb
@@ -1,18 +1,13 @@
 require 'active_record'
 require 'yaml'
 
-LOG = true
-
 db_config = YAML::load(File.open('config/database.yml'))
 
 env_config = if ENV["TEST"]
   db_config["test"]
 else
-  db_config["development"]
-end
-
-if LOG
   ActiveRecord::Base.logger = Logger.new(STDOUT)
+  db_config["development"]
 end
 
 raise "Could not find database config for environment" unless env_config
---
+++ tests.rb
@@ -0,0 +1,43 @@
+require "minitest/autorun"
+require "rack/test"
+
+ENV["TEST"] = "true"
+
+require_relative "./server"
+
+
+class ServerTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    MusicBoxApp
+  end
+
+  def setup
+    User.delete_all
+  end
+
+  def sign_in user
+    # This is a helper method that you can call to sign a particular user in
+    post "/take_sign_in", username: user.name, password: user.password
+  end
+
+  def test_users_can_log_in
+    katie = User.create! name: "Katie", password: "hunter2"
+
+    sign_in katie
+    assert last_response.redirect?
+
+    response = get "/"
+    assert_equal response.status, 200
+    assert_includes response.body, katie.name
+  end
+
+  def test_users_can_fail_to_log_in
+    User.create! name: "user", password: "password"
+
+    response = post "/take_sign_in", username: "Katie", password: "password"
+    refute response.redirect?
+    assert_includes response.body, "Bad username or password"
+  end
+end
