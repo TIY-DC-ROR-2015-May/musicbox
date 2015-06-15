b62585c82702a9aa509906539910207c20c16a36
Dru <drutyper@gmail.com>
2015-06-15 16:53:06 -0400

    added song verify to test

--- tests.rb
+++ tests.rb
@@ -15,6 +15,7 @@ class ServerTest < Minitest::Test
 
   def setup
     User.delete_all
+    Song.delete_all
   end
 
   def sign_in user
@@ -42,18 +43,25 @@ class ServerTest < Minitest::Test
   end
 
   def test_user_signed_in
-      james = User.create! name: "James", password: "hunter2"
-      sign_in james
-      response = get "/"
-      assert_includes response.body, "vote"
-      assert_equal last_response.status, 200 
-      assert_includes response.body, "suggest"
+    james = User.create! name: "James", password: "hunter2"
+    sign_in james
+    song = james.suggested_songs.create! artist: "ODESZA", title: "All We Need"
+    response = get "/"
+    assert_includes response.body, "vote"
+    assert_equal last_response.status, 200 
+    assert_includes response.body, "suggest"
+    assert_includes response.body, "artist: ODESZA, title: All We Need"
   end
 
   def test_user_not_signed_in
-   response = get "/"
-   refute_includes response.body, "vote"
-   refute_includes response.body, "suggest"
+    james = User.create! name: "James", password: "hunter2"
+
+    song = james.suggested_songs.create! artist: "ODESZA", title: "All We Need"
+    response = get "/"
+    refute_includes response.body, "vote"
+    refute_includes response.body, "suggest"
+    assert_includes response.body, song.artist
+    assert_includes response.body, song.title
 
   end
 end
