83a83d83bfe1eaaba69f4f29f644ade6236ff716
Dirk Link <dlink87@MacBook-Pro.local>
2015-06-16 17:52:49 -0400

    added the embedded preview on the song list

--- spotify_api.rb
+++ spotify_api.rb
@@ -16,22 +16,6 @@ class SpotifyAPI
     #@headers = { "Authorization" => "token #{Token}", "User-Agent" => "Wandows Explorer" }
   end
 
-  # def org_members org_name=nil
-  #   # unless org_name
-  #   #   org_name = class_org
-  #   # end
-  #   org_name ||= class_org
-  #   # self.class.get ...
-  #   GithubAPI.get("/orgs/#{org_name}/members")
-  # end
-
-  def repos page=nil, user=nil
-    page ||= 1
-    user ||= "jamesdabbs"
-    # GET /users/jamesdabbs/repos
-    SpotifyAPI.get("/users/#{user}/repos", query: { page: page, per_page: 50 })
-  end
-
   def get_track artist, track
     s = SpotifyAPI.get("/v1/search", query: { q: "artist:#{artist} + track:#{track}", type: "track"}) 
     # query: { q: "uprising", type: "track"}
